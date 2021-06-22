7cd91508195eb58f06702b16a8d6bf5d5093e62e
gil <anovis1999@gmail.com>
2021-06-22 22:53:26 +0200

    Start working on Worker class

--- Hospital/Worker.cs
+++ Hospital/Worker.cs
@@ -6,5 +6,33 @@ namespace Hospital
 {
     class Worker
     {
+        private string name;
+        private string workerid;
+        private string jobname;
+        private string jobtype;
+
+        public Worker(string name, string workerid, string jobname, string jobtype)
+        {
+            this.name = name;
+            this.jobname = jobname;
+            this.jobtype = jobtype;
+            this.workerid = workerid;
+        }
+        public string Name
+        {
+            get { return this.name; }
+        }
+        public string Jobname
+        {
+            get { return this.jobname; }
+        }
+        public string Jobtype
+        {
+            get { return this.jobtype; }
+        }
+        public string Workerid
+        {
+            get { return this.workerid; }
+        }
     }
 }
